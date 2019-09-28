23360a273e4112490bba6084a337cf2a56db1656
rogan528 <3097940262@qq.com>
2019-09-28 22:31:51 +0800

    加载长图

--- app/src/main/java/com/android/bigpicture/MainActivity.java
+++ app/src/main/java/com/android/bigpicture/MainActivity.java
@@ -4,11 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BigView bigView = findViewById(R.id.bigView);
+        InputStream inputStream = null;
+        try {
+            inputStream= getAssets().open("big.png");
+            bigView.setImage(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            if (null != inputStream){
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
     }
 }
